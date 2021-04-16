d74ef0889edaa82b1a977381b9b74d470677ea5e
bigwizzo <wnnwillard@gmail.com>
2021-04-17 00:35:09 +0200

    Add files recommended files

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="h-one">Milestone One</h1>;
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Display from './Display';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Milestone One</h1>
+      <Display result={0} />
+    </div>
+  );
+}
+
+export default App;
