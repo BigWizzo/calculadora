8fc4d0bddf470b7492388dd2f4c446026464fee3
bigwizzo <wnnwillard@gmail.com>
2021-04-21 23:00:29 +0200

    Add state to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <h1>Calculadora: Milestone Five</h1>
-      <Display result="0" />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = (buttonName) => {
+      const result = calculate(this.state, buttonName);
+      this.setState(result);
+    };
+  }
+
+  render() {
+    const { next } = this.state;
+    return (
+      <div className="App">
+        <h1>Milestone 5: Stateful</h1>
+        <Display displayValue={next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
