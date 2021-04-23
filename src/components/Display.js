15f16f9fb6b9998345c16958404f845cc965874e
bigwizzo <wnnwillard@gmail.com>
2021-04-23 20:50:12 +0200

    Refactor the display.js code

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => <div>{result}</div>;
+const Display = ({ result }) => (
+  <div>
+    {result}
+  </div>
+);
 
 Display.propTypes = {
   result: PropTypes.string,
 };
 
 Display.defaultProps = {
-  result: 0,
+  result: '0',
 };
 
 export default Display;
